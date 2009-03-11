3b8afbe7070d953d6c96d531ce56c4ec0b85743e
paulmwatson <github@paulmwatson.com>
2009-03-11 13:34:50 +0000

    New design plus sparklines for cities and phrases.

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -2,5 +2,16 @@ class CitiesController < ApplicationController
   layout 'base'
   def index
     @cities = City.find :all, :order => 'country, name'
+    
+    @mentions = Mention.find :all, :conditions => {:mentioned_at => ("#{Date.today.year}-#{Date.today.month.to_s.rjust(2, '0')}-#{Date.today.day.to_s.rjust(2, '0')} 00:00".."#{Date.today.year}-#{Date.today.month.to_s.rjust(2, '0')}-#{Date.today.day.to_s.rjust(2, '0')} 23:59")}, :order => :mentioned_at
+    
+    @trend = Hash.new()
+    @cities.each do |city|
+      @trend[city.id] = Array.new(24,0)
+    end
+    
+    @mentions.each do |mention|
+      @trend[mention.city_id][mention.mentioned_at.hour] = @trend[mention.city_id][mention.mentioned_at.hour] + 1
+    end
   end
 end
--- app/controllers/phrases_controller.rb
+++ app/controllers/phrases_controller.rb
@@ -1,6 +1,18 @@
 class PhrasesController < ApplicationController
   layout 'base'
   def index
+    @city = City.find :first, :conditions => {:name => params[:city]}
     @phrases = Phrase.find :all, :order => 'title'
+    
+    @mentions = Mention.find :all, :conditions => {:city_id => @city.id, :mentioned_at => ("#{Date.today.year}-#{Date.today.month.to_s.rjust(2, '0')}-#{Date.today.day.to_s.rjust(2, '0')} 00:00".."#{Date.today.year}-#{Date.today.month.to_s.rjust(2, '0')}-#{Date.today.day.to_s.rjust(2, '0')} 23:59")}, :order => :mentioned_at
+    
+    @trend = Hash.new()
+    @phrases.each do |phrase|
+      @trend[phrase.id] = Array.new(24,0)
+    end
+    
+    @mentions.each do |mention|
+      @trend[mention.phrase_id][mention.mentioned_at.hour] = @trend[mention.phrase_id][mention.mentioned_at.hour] + 1
+    end
   end  
 end
