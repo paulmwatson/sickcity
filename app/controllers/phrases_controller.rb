2aae696aba91d4e35406aa923ae4834fa4ee2037
paulmwatson <github@paulmwatson.com>
2009-03-11 23:54:38 +0000

    Design changes aimed at putting the city in SickCity.

--- app/controllers/phrases_controller.rb
+++ app/controllers/phrases_controller.rb
@@ -3,6 +3,7 @@ class PhrasesController < ApplicationController
   def index
     @city = City.find :first, :conditions => {:name => params[:city]}
     @phrases = Phrase.find :all, :order => 'title'
+    @location = get_location(@city.name, @city.country)
     
     @mentions = Mention.find :all, :conditions => {:city_id => @city.id, :mentioned_at => ("#{Date.today.year}-#{Date.today.month.to_s.rjust(2, '0')}-#{Date.today.day.to_s.rjust(2, '0')} 00:00".."#{Date.today.year}-#{Date.today.month.to_s.rjust(2, '0')}-#{Date.today.day.to_s.rjust(2, '0')} 23:59")}, :order => :mentioned_at
     
@@ -14,5 +15,35 @@ class PhrasesController < ApplicationController
     @mentions.each do |mention|
       @trend[mention.phrase_id][mention.mentioned_at.hour] = @trend[mention.phrase_id][mention.mentioned_at.hour] + 1
     end
-  end  
+  end
+  
+  def get_location(city, country)
+    require 'hpricot'
+    require 'open-uri'
+
+    location = Location.find :first, :conditions => {:name => city}
+    if !location
+      begin
+        geocode_url = "http://maps.google.com/maps/geo?q=#{city.gsub(' ', '%20')},#{country.gsub(' ', '%20')}&output=xml&oe=utf8&sensor=false&key=#{GOOGLE_API_KEY}"
+        geocode_doc = open(geocode_url) { |f| Hpricot(f) }
+
+        coords_ele = geocode_doc.search('//coordinates')[0]
+      rescue
+      end
+      
+      if coords_ele
+        coords = coords_ele.inner_html
+      else
+        coords = '0,0'
+      end
+
+      location = Location.new
+      location.name = city
+      location.latitude = coords.split(',')[1]
+      location.longitude = coords.split(',')[0]
+      location.save
+    end
+    location
+  end
+  
 end
