f8bae16be67c40dfd7f21c4f8f91fdadafa93c0f
Aditya Chadha <aditya@sublucid.com>
2009-04-27 14:35:41 -0400

    Add badwords scaffold; and rake task to scrub the db

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :badwords
+
   map.connect '/cities/check/', :controller => 'cities', :action => 'check'
   map.connect '/cities/create/', :controller => 'cities', :action => 'create'
   map.connect '/cities/add/', :controller => 'cities', :action => 'new'
