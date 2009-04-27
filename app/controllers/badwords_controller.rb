76205002412bbc5aa1758a08500b313d93c19073
Aditya Chadha <aditya@sublucid.com>
2009-04-27 16:19:54 -0400

    Skip them bad words; view needs to be prettied up, but works nonetheless

--- app/controllers/badwords_controller.rb
+++ app/controllers/badwords_controller.rb
@@ -1,8 +1,10 @@
 class BadwordsController < ApplicationController
+  layout 'base'
   # GET /badwords
   # GET /badwords.xml
   def index
     @badwords = Badword.find(:all)
+    @badword = Badword.new
 
     respond_to do |format|
       format.html # index.html.erb
@@ -12,43 +14,44 @@ class BadwordsController < ApplicationController
 
   # GET /badwords/1
   # GET /badwords/1.xml
-  def show
-    @badword = Badword.find(params[:id])
+# def show
+#   @badword = Badword.find(params[:id])
 
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @badword }
-    end
-  end
+#   respond_to do |format|
+#     format.html # show.html.erb
+#     format.xml  { render :xml => @badword }
+#   end
+# end
 
-  # GET /badwords/new
-  # GET /badwords/new.xml
-  def new
-    @badword = Badword.new
+# # GET /badwords/new
+# # GET /badwords/new.xml
+# def new
+#   @badword = Badword.new
 
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @badword }
-    end
-  end
+#   respond_to do |format|
+#     format.html # new.html.erb
+#     format.xml  { render :xml => @badword }
+#   end
+# end
 
-  # GET /badwords/1/edit
-  def edit
-    @badword = Badword.find(params[:id])
-  end
+# # GET /badwords/1/edit
+# def edit
+#   @badword = Badword.find(params[:id])
+# end
 
   # POST /badwords
   # POST /badwords.xml
   def create
     @badword = Badword.new(params[:badword])
+    @badword.phrase_id = -1
 
     respond_to do |format|
       if @badword.save
         flash[:notice] = 'Badword was successfully created.'
-        format.html { redirect_to(@badword) }
+        format.html { redirect_to(badwords_url) }
         format.xml  { render :xml => @badword, :status => :created, :location => @badword }
       else
-        format.html { render :action => "new" }
+        format.html { render :action => "index" }
         format.xml  { render :xml => @badword.errors, :status => :unprocessable_entity }
       end
     end
@@ -56,30 +59,30 @@ class BadwordsController < ApplicationController
 
   # PUT /badwords/1
   # PUT /badwords/1.xml
-  def update
-    @badword = Badword.find(params[:id])
+# def update
+#   @badword = Badword.find(params[:id])
 
-    respond_to do |format|
-      if @badword.update_attributes(params[:badword])
-        flash[:notice] = 'Badword was successfully updated.'
-        format.html { redirect_to(@badword) }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @badword.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
+#   respond_to do |format|
+#     if @badword.update_attributes(params[:badword])
+#       flash[:notice] = 'Badword was successfully updated.'
+#       format.html { redirect_to(@badword) }
+#       format.xml  { head :ok }
+#     else
+#       format.html { render :action => "edit" }
+#       format.xml  { render :xml => @badword.errors, :status => :unprocessable_entity }
+#     end
+#   end
+# end
 
-  # DELETE /badwords/1
-  # DELETE /badwords/1.xml
-  def destroy
-    @badword = Badword.find(params[:id])
-    @badword.destroy
+# # DELETE /badwords/1
+# # DELETE /badwords/1.xml
+# def destroy
+#   @badword = Badword.find(params[:id])
+#   @badword.destroy
 
-    respond_to do |format|
-      format.html { redirect_to(badwords_url) }
-      format.xml  { head :ok }
-    end
-  end
+#   respond_to do |format|
+#     format.html { redirect_to(badwords_url) }
+#     format.xml  { head :ok }
+#   end
+# end
 end
